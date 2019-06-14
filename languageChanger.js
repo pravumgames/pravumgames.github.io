edffdca0c90e5893d0137677f0665cf6ee500ed9
Daniel Nesov <d.nesov@gmail.com>
2019-06-14 11:29:08 +0200

    Some fixes

---
+++ languageChanger.js
@@ -0,0 +1,73 @@
+var locale = {
+    lang: {
+        en: {
+            "header": "Hello, we're Pravum",
+            "subHeader": "We're making games!",
+            "dwDesc": "Year 2025, dark age of humanity. We're entering the technological singularity, but the cost of human's freedom. Cities are being monitored by advanced AI, and our race is being treated as slaves: everyone who doesn't work on the factories is being considered as a 'obsolete', and the fate of those is unknown.",
+            "OurTeam": "Our Team",
+            "OurTeamDesc": "We're group of people that was formed by Daniel Nesov and Piotr Horak. We are newcomers to serious game dev, but we all had previous experiences in the past.<br><br>Being driven only by motivation, we're developing our first game: Dyingworld.<br>Our goal is to prove that making a quality game is possible for anyone, no matter the age or team size. We want to make a game, that will be 100% free, without micro transactions, and which will be enjoyable. We won't release it, until we will enjoy it ourselves.<br><br>We think that gameplay + story > visuals. Worlds we create are not always pleasing to the eye and ears, but we can guarantee that they're unique and well written. <br><br>Our team consists of about 10 highly talented people, that weren't able to show their potential. Now we can.",
+            "JoinUs": "Ready to join us?",
+            "JoinUsDesc": "We're group of people that was formed by Daniel Nesov and Piotr Horak. We are newcomers to serious game dev, but we all had previous experiences in the past. <br /><br />Being driven only by motivation, we're developing our first game:<strong>Dyingworld</strong>.<br />Our goal is to prove that making a quality game is possible for anyone, no matter the age or team size. We want to make a game, that will be 100% free, without micro transactions, and which will be enjoyable. We won't release it, until we will enjoy it ourselves.<br /><br />We think that gameplay + story > visuals. Worlds we create are not always pleasing to the eye and ears, but we can guarantee that they're unique and well written.<br /><br />Our team consists of about 10 highly talented people, that weren't able to show their potential. Now we can."
+        },
+        ru: {
+            "header": "Привет, <br> мы Pravum <img src='logo.jpg' width='100px' />",
+            "subHeader": "Мы создаем игры!",
+            "dwDesc": "Год 2025, тёмный век человечества. Мы погружаемся в технологическую сингулярность, ценой человеческой свободы. Города руководствуються продвинутым ИИ, и наша раса теперь как рабы: те, что не хотят работать на фабриках, становяться 'ненужными'. Судьба таковых неизвестна.",
+            "OurTeam": "Наша команда",
+            "OurTeamDesc": "Мы група людей основана благодаря Даниилу Несовому и Пиотру Хораке. Мы новички в серьезной разработке игор, но каждый из нам имел опыт в прошлом. <br /><br />Руководствуясь только мотивацией, мы разрабатываем свою первую игру:<strong>Dyingworld</strong>.<br />Наша цель это доказать, что создать качественную игру может каждый, не смотря на возрасть и размер команды. Мы хотим создыть игру, которая будет 100% бесплатна, без так называемого 'доната' и в которую приятно играть. Мы не выпустим проект пока сами не будем получать удовольствие от игры в него.<br /><br />Мы думаем, что геймплей + сюжет > графика. Может, миры которые мы создаем не отличаються красивыми пейзажами, но мы можем гарантированы, что они будут уникальными и интересными.<br /><br />Наша команда состоит из 10 талантов, которые не могли показать свой истинный потенциал. Теперь они могут.",
+            "JoinUs": "Готовы присоединиться к нам?",
+            "JoinUsDesc": "Вы можете подать заявку на вступление в команду не смотря на возраст, пол, временную зону и страну. Мы открыты для всех тех, кто хотел сделать что-то крутое, но не мог раньше. Мы сделаем все, чтобы вам нравилось делать то, что мы делаем: софт и игры. Поетому мы никогда вас не будем торопить. Мы хотим делать качественный контент, а не быстро собранный на коленках.<br><br>Если вы готовы, вы можете нажать <a href='https://forms.gle/gckvRGeY3AxK5XvV6'>здесь</a>, чтобы заполнить формулу! С нетерпением ждем, чтобы с вами работать!"
+        },
+        pl: {
+            "header": "Cześć, <br> jesteśmy Pravum <img src='logo.jpg' width='100px' />",
+            "subHeader": "Zajmujemy się tworzeniem gier!",
+            "dwDesc": "Rok 2025, ciemny okres ludzkości. Jesteśmy coraz bliżej singularności technologicznej, za co płacimy ludzką wolnością. Miasta są kontrolowane przez zaawansowaną SI, i nasza rasa zamieniła się w niewolników: ci, którzy nie chcą pracować na fabrykach, stają się 'niepotrzebnymi'. Los takowych jest nieznany.",
+            "OurTeam": "Nasz zespół",
+            "OurTeamDesc": "Jesteśmy grupą ludzi założoną przez Daniela Nesova i Piotra Horaka. Dopiero zaczynamy swoją przygodę w poważnym tworzeniu gier. Każdy z nas miał doświadczenie w przeszłości. <br /><br />Pracując tylko dzięki motywacji, tworzymy pierwszą własną grę:<strong>Dyingworld</strong>.<br />Naszym celem jest udowodnienie tego, że każdy może stworzyć jakościową grę, nie zależnie od wieku i rozmiaru teamu. Chcemy stworzyć grę, która będzie 100% darmowa, bez mikropłatności, i w którą po prostu przyjemnie grać. Nie wydamy projektu, dopóki sami nie będziemy otrzymywać zabawę z niego.<br /><br />Myślimy, że rozgrywka + fabuła > grafika. Może światy które tworzymy nie są piękne w oprawie wizualnej, lecz możemy gwarantować, że te światy będą unikatowe i ciekawe.<br /><br />Składamy się z około 10 talentów, którzy dotychczas nie mogli ujawnić własnego potencjału. Teraz mają możliwość, by to zrobić.",
+            "JoinUs": "Jesteś gotów dołączyć do nas?",
+            "JoinUsDesc": "Każdy może wypełnić ankietę nie ważne jaki masz wiek, płeć, strefę czasową i kraj zamieszkania, jesteśmy otwarci dla wszystkich, co chcieli zrobić coś, co jest niesamowitego, lecz nie mógł tego zrobić wcześniej. Zrobimy wszystko, by wam podobało się robić to, co i nam: tworzyć soft i gry. Ze względu na to, nie będziemy cię siłować robić coś szybciej. Chcemy robić jakościowe produkcje, nie na 'odwal się'.<br><br>Jeżeli jesteś gotów, możesz kliknąć <a href='https://forms.gle/gckvRGeY3AxK5XvV6'>tu</a>, by wypełnić formularz! Nie możemy się doczekać, aż będziemy pracować razem!"
+        }
+}
+}
+
+function changeLang(target) {
+    //I'm such a good programmer xD
+
+    var header = document.getElementById("header")
+    var subHeader = document.getElementById("subHeader")
+    var dwDesc = document.getElementById("dwDesc")
+    var OurTeam = document.getElementById("OurTeam")
+    var OurTeamDesc = document.getElementById("OurTeamDesc")
+    var JoinUs = document.getElementById("JoinUs")
+    var JoinUsDesc = document.getElementById("JoinUsDesc")
+
+    if(target == "en") {
+        header.innerHTML = locale.lang.en.header
+        subHeader.innerHTML = locale.lang.en.subHeader
+        dwDesc.innerHTML = locale.lang.en.dwDesc
+        OurTeam.innerHTML = locale.lang.en.OurTeam
+        OurTeamDesc.innerHTML = locale.lang.en.OurTeamDesc
+        JoinUs.innerHTML = locale.lang.en.JoinUs
+        JoinUsDesc.innerHTML = locale.lang.en.JoinUsDesc
+    }
+
+    if(target == "ru") {
+        header.innerHTML = locale.lang.ru.header
+        subHeader.innerHTML = locale.lang.ru.subHeader
+        dwDesc.innerHTML = locale.lang.ru.dwDesc
+        OurTeam.innerHTML = locale.lang.ru.OurTeam
+        OurTeamDesc.innerHTML = locale.lang.ru.OurTeamDesc
+        JoinUs.innerHTML = locale.lang.ru.JoinUs
+        JoinUsDesc.innerHTML = locale.lang.ru.JoinUsDesc
+    }
+
+    if(target == "pl") {
+        header.innerHTML = locale.lang.pl.header
+        subHeader.innerHTML = locale.lang.pl.subHeader
+        dwDesc.innerHTML = locale.lang.pl.dwDesc
+        OurTeam.innerHTML = locale.lang.pl.OurTeam
+        OurTeamDesc.innerHTML = locale.lang.pl.OurTeamDesc
+        JoinUs.innerHTML = locale.lang.pl.JoinUs
+        JoinUsDesc.innerHTML = locale.lang.pl.JoinUsDesc
+    }
+}
